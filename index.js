063001427ba56af3a1afdf00f24ca7484bd9ef9b
iSaacSigei <sigei.isack@student.moringaschool.com>
2022-08-18 20:55:32 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+//function expression
+const divide=function (x=2000,y=100){
+    let z=x/y;
+    return z
+}
+
+//arrow function II
+const square=(y)=>y*y;
+
+//Arrow function
+const add=(num1,num2)=>num1+num2;
